94ddc50f86b21ea18e2a98f32bac4ff14f1c4ca3
Kurai András <k.andras9642@gmail.com>
2020-04-22 20:10:10 +0200

    Add projects

---
+++ GeneratorUtils.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeneratorUtils.Cli
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
