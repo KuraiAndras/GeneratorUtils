a7af316494b22c8f0ae1d4ba0ebf493e9066843a
Kurai András <k.andras9642@gmail.com>
2020-04-23 20:32:27 +0200

    Add first test

---
+++ src/GeneratorUtils.Cli/Handlers/MediatR/GenerateMediatRRequestHandlersHandler.cs
@@ -0,0 +1,15 @@
+﻿using GeneratorUtils.Cli.Requests.MediatR;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GeneratorUtils.Cli.Handlers.MediatR
+{
+    public sealed class GenerateMediatRRequestHandlersHandler : IRequestHandler<GenerateMediatRRequestHandlers>
+    {
+        public Task<Unit> Handle(GenerateMediatRRequestHandlers request, CancellationToken cancellationToken)
+        {
+            return Unit.Task;
+        }
+    }
+}
